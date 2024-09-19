a18785e0025782c7d585d1f1b9314e5e1f84a2ef
mistimalaviya <163491405+mistimalaviya@users.noreply.github.com>
2024-09-19 12:25:14 +0530

    Add files via upload

---
+++ form validation/script.js
@@ -0,0 +1,17 @@
+// const signInBtnlink = document.querySelector('.signInBtnlink');
+// const signuplink = document.querySelector('.signupBtnlink');
+// const wrapper = document.querySelector('.wrapper');
+
+document.querySelector('#we').addEventListener("click",()=>{
+var box2=document.querySelector('#box2').style.display="none"
+var box1=document.querySelector('#box1').style.display="block"
+
+})
+
+document.querySelector('#we2').addEventListener("click",()=>{
+    var box2=document.querySelector('#box1').style.display="none"
+    var box1=document.querySelector('#box2').style.display="block"
+    
+    })
+
+
